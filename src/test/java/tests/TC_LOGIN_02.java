38ab96413adad0899daa747f8b9e82dd6e970a48
Dell 7490 <dphuonganh019@gmail.com>
2026-09-06 23:26:06 +0700

    Add TC_LOGIN_02 skeleton for empty username login failure test

---
+++ src/test/java/tests/TC_LOGIN_02.java
@@ -0,0 +1,9 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import pages.LoginPage;
+
+@DisplayName("TC_LOGIN_02: Login fails - empty Username")
+public class TC_LOGIN_02 extends BaseTest {
+    static LoginPage loginPage;
+}
