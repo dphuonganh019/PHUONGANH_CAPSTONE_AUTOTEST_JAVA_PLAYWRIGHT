74b68303a8eb0063033412b80e3864201d69a802
Dell 7490 <dphuonganh019@gmail.com>
2026-09-06 23:25:24 +0700

    Add LoginPage object for the SauceDemo login screen

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,134 @@
+package pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import utils.ConfigReader;
+import utils.LogUtil;
+
+import java.util.List;
+
+public class LoginPage extends BasePage {
+    private final Locator loginLogo = page.locator("//div[@class='login_logo']");
+    private final Locator usernameInput = page.locator("//input[@data-test='username']");
+    private final Locator passwordInput = page.locator("//input[@data-test='password']");
+    private final Locator loginButton = page.locator("//input[@data-test='login-button']");
+    private final Locator accountList = page.locator("//div[@data-test='login-credentials']");
+    private final Locator passwordList = page.locator("//div[@data-test='login-password']");
+    private final Locator loginBox = page.locator("//div[@class='login-box']");
+
+    public LoginPage(Page page){
+        super(page);
+    }
+
+    public void goToSauceDemoPage(){
+        navigateToUrl(ConfigReader.get("baseUrl"));
+    }
+
+    public boolean isLoginLogoVisible() {
+        return isElementVisible(loginLogo);
+    }
+
+    public String getLogoText() {
+        LogUtil.info("Logo text: " + getInnerText(loginLogo));
+        return getInnerText(loginLogo);
+    }
+
+    public boolean isUsernameInputVisible() {
+        return isElementVisible(usernameInput);
+    }
+
+    public String getUsernamePlaceHolder() {
+        LogUtil.info("Username placeholder: " + getPlaceHolderValue(usernameInput));
+        return getPlaceHolderValue(usernameInput);
+    }
+
+    public String getUsernameInputValue() {
+        LogUtil.info("Current input value: " + getAttributeValue(usernameInput));
+        return getAttributeValue(usernameInput);
+    }
+
+    public String getPasswordPlaceHolder() {
+        LogUtil.info("Password placeholder: " + getPlaceHolderValue(passwordInput));
+        return getPlaceHolderValue(passwordInput);
+    }
+
+    public String getPasswordInputValue() {
+        LogUtil.info("Current input value: " + getAttributeValue(usernameInput));
+        return getAttributeValue(usernameInput);
+    }
+
+    public boolean isPasswordInputVisible() {
+        return isElementVisible(passwordInput);
+    }
+
+    public boolean isLoginButtonVisible() {
+        return isElementVisible(loginButton);
+    }
+
+    public boolean isLoginButtonEnable() {
+        return isElementEnabled(loginButton);
+    }
+
+    public String getLoginButtonText() {
+        LogUtil.info("Login button text: " + getAttributeValue(loginButton));
+        return getAttributeValue(loginButton);
+    }
+
+    public boolean isAccountListVisible() {
+        return isElementVisible(accountList);
+    }
+
+    public boolean isPasswordListVisible() {
+        return isElementVisible(passwordList);
+    }
+
+    public void fillUsername(String username) {
+        fillText(usernameInput, username);
+        LogUtil.info("Filling username field: " + getAttributeValue(usernameInput));
+    }
+
+    public void fillPassword(String password) {
+        fillText(passwordInput, password);
+    }
+
+    public void clickLoginButton() {
+        clickElement(loginButton);
+    }
+
+    public String getListAccount() {
+        LogUtil.info("List account text: " + getInnerText(accountList));
+        return getInnerText(accountList);
+    }
+
+    public String getListPassword() {
+        return getInnerText(passwordList);
+    }
+
+    public boolean isUsernameFieldInLoginBox() {
+        return loginBox.locator(usernameInput).count() == 1;
+    }
+
+    public boolean isPasswordFieldInLoginBox() {
+        return loginBox.locator(passwordInput).count() == 1;
+    }
+
+    public boolean isLoginButtonInLoginBox() {
+        return loginBox.locator(loginButton).count() == 1;
+    }
+
+    public double getLoginFieldPositionY() {
+        LogUtil.info("Login field position: " + getElementYPosition(usernameInput));
+        return getElementYPosition(usernameInput);
+    }
+
+    public double getPasswordFieldPositionY() {
+        LogUtil.info("Password field position: " + getElementYPosition(passwordInput));
+        return getElementYPosition(passwordInput);
+    }
+
+    public double getLoginButtonPositionY() {
+        LogUtil.info("Login button position: " + getElementYPosition(loginButton));
+        return getElementYPosition(loginButton);
+    }
+
+}
