114d91dcc66edd975884bc48ea584d9464fc4917
Dell 7490 <dphuonganh019@gmail.com>
2026-09-06 23:26:06 +0700

    Add TC_LOGIN_01: successful login test with URL redirect verification

---
+++ src/test/java/tests/TC_LOGIN_01.java
@@ -0,0 +1,68 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+import utils.ConfigReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("TC_LOGIN_01: Successful login with standard_user")
+public class TC_LOGIN_01 extends BaseTest {
+    static LoginPage loginPage;
+
+    private final static String EXPECTED_URL_AFTER_LOGIN = ConfigReader.get("baseUrl") + "inventory.html";
+
+    @BeforeAll
+    static void setup() {
+        loginPage = new LoginPage(page);
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Step 1: Navigate to https://www.saucedemo.com/</br>")
+    public void navigateToSauceDemo() {
+        loginPage.goToSauceDemoPage();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Step 2: Enter 'standard_user' into the Username field")
+    public void enterTheUsername() {
+        loginPage.fillUsername(ConfigReader.get("username_standard_user"));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Step 3: Enter value into the Password field")
+    public void enterThePassword() {
+        loginPage.fillPassword(ConfigReader.get("password"));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Step 4: Click the Login button")
+    public void clickLoginButton() {
+        loginPage.clickLoginButton();
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Expected Result: System redirects to https://www.saucedemo.com/inventory.html")
+    public void verifyLoginSuccessfully() {
+        final String actualUrl = browserManager.getCurrentTabUrl();
+        assertEquals(
+                EXPECTED_URL_AFTER_LOGIN,
+                actualUrl,
+                "The system did not redirect to the expected URL. Expected: ["
+                        + EXPECTED_URL_AFTER_LOGIN
+                        + "], Actual: ["
+                        +  actualUrl
+                        + "]"
+        );
+    }
+
+
+}
