ce47083927ebca1faacfda528419b7a144c753f7
Dell 7490 <dphuonganh019@gmail.com>
2026-09-06 23:26:08 +0700

    Add JUnit5 suite to run the login test classes together

---
+++ src/test/java/tests/EndToEndSystemTestSuite.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        TC_LOGIN_01.class,
+        TC_LOGIN_10.class
+})
+public class EndToEndSystemTestSuite {
+}
